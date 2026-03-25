ba505b7fdc365082ed5455968ae602313c19b9bf
SatishYedida-hub <satishyedida36@gmail.com>
2026-03-25 17:30:41 +0530

    Update ai_review.py

--- ai_review.py
+++ ai_review.py
@@ -1,38 +1,12 @@
-import boto3
-import json
+import openai
 
-client = boto3.client("bedrock-runtime", region_name="us-east-1")
+openai.api_key = "YOUR_API_KEY"
 
-with open("changes.diff", "r") as f:
-    code_changes = f.read()
-
-prompt = f"""
-You are a senior Node.js code reviewer.
-
-Analyze the following code and provide:
-1. Security issues
-2. Bugs
-2. Best practices
-
-Code:
-{code_changes}
-"""
-
-response = client.invoke_model(
-    modelId="anthropic.claude-3-haiku-20240307-v1:0",
-    body=json.dumps({
-        "anthropic_version": "bedrock-2023-05-31",
-        "messages": [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        "max_tokens": 300
-    })
+response = openai.ChatCompletion.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "user", "content": prompt}
+    ]
 )
 
-result = json.loads(response['body'].read())
-
-print("\n===== AI REVIEW =====\n")
-print(result)
+print(response['choices'][0]['message']['content'])
